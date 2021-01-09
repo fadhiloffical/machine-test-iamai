14f94ed2e4de421ceaa0700197e0e22aa3b57396
fadhilasharaf1996 <fadhilasharaf@gmail.com>
2021-01-09 14:58:56 +0530

    Final Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [experiencePoints, setExperiencePoints] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+  const [finalEvenValues, setFinalEvenValues] = useState([]);
+
+  const addExperiencePoint = (e) =>{
+    e.preventDefault();
+    if (searchTerm.length === 0) {
+      alert("Please enter experience")
+    }
+    else{
+      setExperiencePoints([ ...experiencePoints, {
+          id: experiencePoints.length,
+          value: searchTerm
+      } ])
+      setSearchTerm("")
+    }
+  }
+
+  const handleChange = event => {
+      setSearchTerm(event.target.value);
+  };
+  
+  let result;
+const FinalResult = (e1) =>{
+  e1.preventDefault();
+  setFinalEvenValues(experiencePoints);
+
+  setExperiencePoints('');
+
+}
+
+if (finalEvenValues.length > 0) {
+  result = (
+    <>
+    <div className="Result">
+      <h3>Result</h3>
+          <p>
+          {finalEvenValues.filter(finalEvenValues => finalEvenValues.value %2 == 0).map(filteredfinalEvenValues => (
+            <>{filteredfinalEvenValues.value} &nbsp;</>
+          ))}
+          </p>
+
+          <p>
+          {finalEvenValues.filter(finalEvenValues => finalEvenValues.value %2 != 0).map(filteredfinalEvenValues => (
+            <>{filteredfinalEvenValues.value} &nbsp;</>
+          ))}
+          </p>
+    </div>
+    </>
+  )
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="TestForm">
+        
+      <form onSubmit={addExperiencePoint}>
+          <input type="number"
+              placeholder="Experience Point"
+              value={searchTerm}
+              onChange={handleChange} 
+              /><br/><br/>
+
+          <div className="Buttons">
+            <button type="submit">ADD Experience Points</button>
+            <button onClick={FinalResult}>Submit</button>
+          </div>
+          </form>
+
+          {result}
+
+      </div>
+
     </div>
   );
 }
